925cf3373aa3c7a7a568f385d4c7925e493d621e
Jesus Bossa <x@jesusbossa.dev>
2021-09-09 17:18:46 -0500

    chore: test importe components ci

--- src/__tests__/Counter.spec.tsx
+++ src/__tests__/Counter.spec.tsx
@@ -3,10 +3,18 @@ import React from 'react';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import renderer from 'react-test-renderer';
+import TestContainer from '../renderer/components/TestContainer';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe('Counter Component', () => {
+describe('Imported Component', () => {
+  it('should match exact snapshot', () => {
+    const tree = renderer.create(<TestContainer />).toJSON();
+    expect(tree.props.className).toEqual('hoho');
+  });
+});
+
+describe('Hard Component', () => {
   it('should match exact snapshot', () => {
     const counter = (
       <div className="hoho">
---
+++ src/renderer/components/TestContainer.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+
+function TestContainer() {
+  return (
+    <div className="hoho">
+      <h1>Hola</h1>
+    </div>
+  );
+}
+
+export default TestContainer;
